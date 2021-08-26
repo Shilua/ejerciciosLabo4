c31ea227eceb5dd7cccf834516827aa168d72cf2
Shilua <Shilua4@gmail.com>
2021-08-26 20:22:22 -0300

    change in server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'angularapp'));
+app.use(express.static(__dirname+'/angularapp'));
 
 app.listen(process.env.PORT || 8080);
 
